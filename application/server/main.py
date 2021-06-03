c94169bb049cef75d6f115510c0fac33a4a06214
Sameer More <55289526+sameerr7@users.noreply.github.com>
2021-06-03 12:20:01 +0530

    Update main.py

--- application/server/main.py
+++ application/server/main.py
@@ -7,8 +7,8 @@ from application.schema import Symptom
 from application.components.prediction import symptom_check
 
 app_desc = """<h2>Try this app by uploading any image with `predict/image`</h2>
-<h2>Try Covid symptom checker api - it is just a learning app demo</h2>
-<br>by Aniket Maurya"""
+<h2>Try Covid symptom checker api </h2>
+<br>CC Project"""
 
 app = FastAPI(title='Tensorflow FastAPI Starter Pack', description=app_desc)
 
